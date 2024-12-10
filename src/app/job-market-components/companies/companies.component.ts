7ede11de796db180323906ce8e3fe073c82a3405
Abhishek Kumar Gupta <abhishek.gupta1@synoriq.in>
2024-12-10 23:46:10 +0530

    companies page design

--- src/app/job-market-components/companies/companies.component.ts
+++ src/app/job-market-components/companies/companies.component.ts
@@ -12,46 +12,58 @@ export class CompaniesComponent implements OnInit {
 
   companies = [
     {
+      id: 1,
       name: 'Tech Corp',
       logo: 'https://via.placeholder.com/100',
       industry: 'IT & Services',
       openPositions: 15,
       location: 'Mumbai',
+      establishYear : 2007,
     },
     {
+      id: 2,
       name: 'Fin Bank',
       logo: 'https://via.placeholder.com/100',
       industry: 'Finance',
       openPositions: 20,
       location: 'Bangalore',
+      establishYear : 2010,
     },
     {
+      id: 3,
       name: 'HealthPlus',
       logo: 'https://via.placeholder.com/100',
       industry: 'Healthcare',
       openPositions: 10,
       location: 'Delhi',
+      establishYear : 2000,
     },
     {
+      id: 4,
       name: 'Tech Corp',
       logo: 'https://via.placeholder.com/100',
       industry: 'IT & Services',
       openPositions: 15,
       location: 'Mumbai',
+      establishYear : 2008,
     },
     {
+      id: 5,
       name: 'Fin Bank',
       logo: 'https://via.placeholder.com/100',
       industry: 'Finance',
       openPositions: 20,
       location: 'Bangalore',
+      establishYear : 2002,
     },
     {
+      id: 6,
       name: 'HealthPlus',
       logo: 'https://via.placeholder.com/100',
       industry: 'Healthcare',
       openPositions: 10,
       location: 'Delhi',
+      establishYear : 2005,
     },
   ];
 
