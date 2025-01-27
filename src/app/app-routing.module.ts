0a3b7060c572592e93539a62d3a29bfc766878f4
Abhishek Kumar Gupta <abhishek.gupta1@synoriq.in>
2025-01-27 21:41:46 +0530

    routing protection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from "./auth.guard";
 import { DashboardComponent } from './job-market-components/dashboard/dashboard.component';
 import { RegistrationComponent } from './job-market-components/registration/registration.component';
 import { LoginComponent } from './job-market-components/login/login.component';
@@ -15,46 +16,55 @@ const routes: Routes = [
   {
     path: "dashboard",
     component: DashboardComponent,
-    data: { title : 'Dashboard' }
+    // canActivate: [AuthGuard], // Protect this route
+    data: { title: 'Dashboard' },
   },
   {
     path: "registration",
     component: RegistrationComponent,
-    data: { title : 'Reistration Form' }
+    data: { title: 'Registration Form' },
   },
   {
     path: "login",
     component: LoginComponent,
-    data: { title : 'Login Form' }
+    data: { title: 'Login Form' },
   },
   {
     path: "jobs",
     component: JobsComponent,
-    data: { title : 'Login Form' }
+    canActivate: [AuthGuard], // Protect this route
+    data: { title: 'Jobs' },
   },
   {
-    path: 'job-detail/:id',
-    component: JobDetailsComponent
+    path: "job-detail/:id",
+    component: JobDetailsComponent,
+    canActivate: [AuthGuard], // Protect this route
   },
   {
-    path: 'services',
-    component: ServicesComponent
+    path: "services",
+    component: ServicesComponent,
+    canActivate: [AuthGuard],
+    data: { title: 'Services' },
   },
   {
-    path: 'companies',
-    component: CompaniesComponent
+    path: "companies",
+    component: CompaniesComponent,
+    canActivate: [AuthGuard], // Protect this route
   },
   {
-    path: 'post-job',
-    component: PostJobComponent
+    path: "post-job",
+    component: PostJobComponent,
+    canActivate: [AuthGuard], // Protect this route
   },
   {
-    path: 'profile',
-    component: ProfileComponent
+    path: "profile",
+    component: ProfileComponent,
+    canActivate: [AuthGuard], // Protect this route
   },
   {
-    path: 'edit-profile',
-    component: EditProfileComponent
+    path: "edit-profile",
+    component: EditProfileComponent,
+    canActivate: [AuthGuard], // Protect this route
   },
   {
     path: "**",
